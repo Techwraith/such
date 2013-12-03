68bcb7d927a43d1a7a8e22af7fbc009a230572d3
Ben Atkin <ben@benatkin.com>
2013-12-03 01:46:57 -0700

    very push and pull

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var cmd = process.argv[2];
 var args = process.argv.slice(2);
 var prg;
 
-if (['init', 'add', 'commit', 'clone', 'checkout', 'status', 'diff', 'rm', 'mv'].indexOf(cmd) != -1) {
+if (['init', 'add', 'commit', 'clone', 'checkout', 'status', 'diff', 'rm', 'mv', 'push', 'pull'].indexOf(cmd) != -1) {
   prg = 'git';
 } else if (['console', 'server', 'generate'].indexOf(cmd) != -1) {
   prg = 'rails';
